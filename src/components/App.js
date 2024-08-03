0c67491c67ed911846dace73356f50a12fd2b6d0
shalyne23 <shalynelangat@gmail.com>
2024-08-03 16:36:31 +0300

    article

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import Header from "./Header";
+import ArticleList from "./ArticleList";
+
+
 
 console.log(blogData);
 
@@ -8,6 +13,11 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <About />
+      <Header />
+      <ArticleList />
+      
+      
     </div>
   );
 }
---
+++ src/components/Article.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ArticleList from "./ArticleList";
+
+const Article = ()=>{
+    return(
+       <ArticleList/>
+    );
+}
+
+export default Article
